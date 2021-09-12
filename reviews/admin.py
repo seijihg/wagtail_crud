332032c6f9a97c6fac9dcc7c68e06082481bda82
Le H Ngo <hgle.ngo@gmail.com>
2021-09-12 21:25:53 +0100

    - Registers review admin model.

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,12 @@
 from wagtail.contrib.modeladmin.options import ModelAdmin, modeladmin_register
+from .models import Review
 
-# Register your models here.
+
+@modeladmin_register
+class ReviewAdmin(ModelAdmin):
+    model = Review
+    menu_label = "Reviews"
+    menu_icon = "placeholder"
+    menu_order = 300
+    add_to_settings_menu = False
+    exclude_from_explorer = False
